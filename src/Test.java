eef5d7e4e132116660bc17a16f379615cd05a829
DESKTOP-3PB1UB5\giuse <giuseppe.catalano.85@gmail.com>
2021-07-05 19:01:52 +0200

    commit 3

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,7 @@ public class Pippo {
 	public static void main (String [] args){
 		Sysout.out.println("Prima stampa");
 		Sysout.out.println("Print 2");
-		Sysout.out.println("Print 3");
+		Sysout.out.println("Print 3");		
+		Sysout.out.println("Print 4");	
 	}
 }
